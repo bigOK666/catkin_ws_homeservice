cb6ae9758793793a06bcc44e8a5b21ff0d6c2944
zerenlu <zeren.lu@gmail.com>
2019-05-15 22:38:26 +0200

    build and link the packages

---
+++ build/turtlebot/turtlebot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kevin/workspace/catkin_ws_homeservice/src/turtlebot/turtlebot_bringup
+# Build directory: /home/kevin/workspace/catkin_ws_homeservice/build/turtlebot/turtlebot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot/turtlebot_capabilities/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kevin/workspace/catkin_ws_homeservice/src/turtlebot/turtlebot_capabilities
+# Build directory: /home/kevin/workspace/catkin_ws_homeservice/build/turtlebot/turtlebot_capabilities
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
