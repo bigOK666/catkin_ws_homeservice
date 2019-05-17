9a87d622366ff653adb327b34d78ef8dfc062213
root <zeren.lu@gmail.com>
2019-05-17 09:31:31 +0000

    add packates

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch;bash" &
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch;bash" &
 
 sleep 5
 
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch;bash" &
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch;bash" &
 
 sleep 5
 
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch;bash" &
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch;bash" &
 
 sleep 5
 
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; rosrun pick_objects pick_objects;bash" &
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects;bash" &
 
 sleep 1
 
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; rosrun add_markers add_markers;bash"
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; rosrun add_markers add_markers;bash"
--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch;bash" &
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch;bash" &
 
 sleep 5
 
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch;bash" &
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch;bash" &
 
 sleep 5
 
-xterm -e " source /home/kevin/workspace/catkin_ws_homeservice/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch;bash" 
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch;bash" 
 
 sleep 5
 
